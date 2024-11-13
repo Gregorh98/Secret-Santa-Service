9b86fe7ba08c72c5fc26ad81607bde6307e22a52
Gregor <gregor.hastings@flexitricity.com>
2024-11-13 16:44:06 +0000

    Updated

--- Secret Santa Service.py
+++ Secret Santa Service.py
@@ -5,61 +5,62 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
 from dotenv import load_dotenv
-import csv
 
 load_dotenv()
 
-santas = csv.reader(open("santa_name_email.csv", encoding="utf8"))
-
 sender = os.getenv("sender")
+password = os.getenv("password")
 
 santas = [
-	["TEST 1","test1@test.com"],
-    ["TEST 2","test2@test.com"]
+    ["Santa 1", "santa1@northpole.com"],
+    ["Santa 2", "santa2@southpole.com"],
 ]
 
 buyingFor = [santa[0] for santa in santas]
 
 matches = []
 
-def matchSantas():
-	for santa in santas:
-		foundMatch = False
-		while foundMatch != True:
-			match = random.choice(buyingFor)
-			if match != santa[0]:
-				buyingFor.remove(match)
-				matches.append([santa, match])
-				foundMatch = True
 
-def getDaysTillChristmas():
+def match_santas():
+    for santa in santas:
+        foundMatch = False
+        while not foundMatch:
+            match = random.choice(buyingFor)
+            if match != santa[0]:
+                buyingFor.remove(match)
+                matches.append([santa, match])
+                foundMatch = True
+
+
+def get_days_till_christmas():
     today = datetime.today()
     christmas = datetime(today.year, 12, 25)
-    
+
     # Check if Christmas has already passed this year
     if today > christmas:
-	    christmas = datetime(today.year + 1, 12, 25)
-    
+        christmas = datetime(today.year + 1, 12, 25)
+
     days_till_christmas = (christmas - today).days
     return days_till_christmas
 
 
-def emailSantas():
-		print("Opening SMTP server")
-		mailserver = smtplib.SMTP('smtp.office365.com',587)
-		mailserver.ehlo()
-		print("Encrypting transmissions")
-		mailserver.starttls()
-		mailserver.ehlo()
-		print("Logging in")
-		mailserver.login(sender, 'lG4M#@0WTB&y')
-		
-		for match in matches:
-			msg = MIMEMultipart()
-			msg['From'] = sender
-			msg['To'] = match[0][1]
-			msg['Subject'] = 'Secret Santa 2023!'
-			message = """
+def email_santas(test=True):
+    print("Opening SMTP server")
+    mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+    mailserver.ehlo()
+    print("Encrypting transmissions")
+    mailserver.starttls()
+    mailserver.ehlo()
+    print("Logging in")
+    mailserver.login(sender, password)
+
+    for match in matches:
+        msg = MIMEMultipart()
+        msg['From'] = sender
+        msg['To'] = match[0][1] if not test else sender
+        msg['Subject'] = f'Secret Santa {datetime.today().year}!'
+        message = f"""
+{"*** THIS IS THE FINAL EMAIL - DISREGARD ALL OTHERS ***" if test else ""}
 Hello Santa %s!
 Its officially %s days till Christmas and its time for Secret Santa %s!
 			
@@ -69,19 +70,20 @@ YOU ARE BUYING FOR %s!
 			
 Merry Christmas, and happy present hunting!
 			""" % (
-			    match[0][0],
-			    getDaysTillChristmas(),
-			    datetime.today().year,
-			    match[1].upper()
-			)
-			
-			msg.attach(MIMEText(message))
+            match[0][0],
+            get_days_till_christmas(),
+            datetime.today().year,
+            match[1].upper()
+        )
 
-			print("Sending mail to %s - %s" % (match[0][0], match[0][1]))
-			#print("\n\n" + message + "\n\n")
-			mailserver.sendmail('sender@test.com',match[0][1],msg.as_string())
-			
-		mailserver.quit()
-				
-matchSantas()
-emailSantas()
+        msg.attach(MIMEText(message))
+
+        print("Sending mail to %s - %s" % (match[0][0], match[0][1] if not test else sender))
+        mailserver.sendmail(sender, match[0][1] if not test else sender, msg.as_string())
+        print(message + "\n\n")
+
+    mailserver.quit()
+
+
+match_santas()
+email_santas(test=False)
